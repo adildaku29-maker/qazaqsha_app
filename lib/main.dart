19b7a8afd4f110c48de135c685870b3aab91fa2b
adildaku29-maker <adildaku29@gmail.com>
2026-09-18 16:49:10 +0600

    Qazaqsha 2.0: update lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,5 @@
 import 'package:flutter/material.dart';
-import 'models/app_models.dart';
-import 'screens/main_navigation_screen.dart';
-import 'screens/onboarding_screen.dart';
-import 'services/storage_service.dart';
 import 'theme/app_theme.dart';
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final player = await StorageService.getPlayer();
-  runApp(QazaqshaApp(initialPlayer: player));
-}
-
-class QazaqshaApp extends StatelessWidget {
-  final Player? initialPlayer;
-  const QazaqshaApp({super.key, this.initialPlayer});
-
-  @override
-  Widget build(BuildContext context) => MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Qazaqsha',
-        theme: AppTheme.theme,
-        home: initialPlayer == null
-            ? const OnboardingScreen()
-            : MainNavigationScreen(player: initialPlayer!),
-      );
-}
+import 'screens/main_navigation_screen.dart';
+void main(){WidgetsFlutterBinding.ensureInitialized();runApp(const QazaqshaApp());}
+class QazaqshaApp extends StatelessWidget{const QazaqshaApp({super.key});@override Widget build(BuildContext c)=>MaterialApp(debugShowCheckedModeBanner:false,title:'Qazaqsha',theme:AppTheme.dark(),home:const MainNavigationScreen());}
