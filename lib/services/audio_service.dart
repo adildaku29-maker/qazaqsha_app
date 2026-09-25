6709e597b32fe75d1ab89f25a97a4fb1ddcdb376
adildaku29-maker <adildaku29@gmail.com>
2026-09-25 18:01:00 +0600

    Add local Kazakh lesson audio service

---
+++ lib/services/audio_service.dart
@@ -0,0 +1,19 @@
+import 'package:audioplayers/audioplayers.dart';
+
+class AudioService {
+  AudioService._();
+  static final AudioService instance = AudioService._();
+  final AudioPlayer _player = AudioPlayer();
+
+  Future<void> speakAsset(String name) async {
+    if (name == 'default') return;
+    try {
+      await _player.stop();
+      await _player.play(AssetSource('audio/tanisu/$name.mp3'));
+    } catch (_) {
+      // Audio files are supplied separately; missing audio must not break the lesson.
+    }
+  }
+
+  Future<void> dispose() => _player.dispose();
+}
